c9ed96a2ed8916e510f4f38cb5c001f013ed1265
Diogo dos Santos Goncalves <diogo_santos_goncalves@hotmail.com>
2021-02-24 13:11:09 -0300

    Commit Final

--- ApiCoreDio/ApiCoreDio/Startup.cs
+++ ApiCoreDio/ApiCoreDio/Startup.cs
@@ -25,22 +25,16 @@ namespace ApiCoreDio
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<Data.MongoDB>();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
